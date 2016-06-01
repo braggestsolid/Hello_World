f3be69b8546e8bf14767f9775ff338c243de2a89
braggestsolid <braggestsolid65@gmail.com>
2016-05-31 20:41:02 -0400

    Modified strings

--- calc.py
+++ calc.py
@@ -28,13 +28,16 @@ print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
+#changed to raw_input over standard input to avoid invalid string.
+
 choice = raw_input("Enter choice(1/2/3/4):")
 
 num1 = int(raw_input("Enter first number: "))
 num2 = int(raw_input("Enter second number: "))
 
 if choice == '1':
-   print(num1,"+",num2,"=", add(num1,num2))
+   #added single instead of double quote.
+   print(num1,'+',num2,'=', add(num1,num2))
 
 elif choice == '2':
    print(num1,"-",num2,"=", subtract(num1,num2))
